2dccfed754ea1e3a960ab6fe8caf8d4ffb83c223
Асхен <Asshen4522@yandex.ru>
2026-05-09 01:17:14 +0300

    newConf

--- src/route.js
+++ src/route.js
@@ -3,8 +3,8 @@ import Page1 from "./components/page1.vue";
 import Page2 from "./components/page2.vue";
 
 const routes = [
-    {path:'/',component: Page1,name:'p1'},
-    {path:'/one',component: Page2,name:'p2'},
+    {path:'',component: Page1,name:'p1'},
+    {path:'one',component: Page2,name:'p2'},
 ]
 
 export const router = createRouter({
