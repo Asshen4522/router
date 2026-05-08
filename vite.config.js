1b159eab4cbdb0d21fd41e07270c52cb52c28cd7
Асхен <Asshen4522@yandex.ru>
2026-05-09 01:36:12 +0300

    newConf

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/router/',
+  // base: '/router/',
+  base: process.env.NODE_ENV === 'production' ? '/router/' : '/'
 })
